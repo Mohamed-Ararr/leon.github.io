a4c442fc848b57d0c101cfc2d7bb8bcec988d530
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-02-23 14:23:30 +0000

    add scroller components

---
+++ javascript/master.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  // add scroller to the website
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scroller = document.querySelector(".scroller");
+
+  window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    scroller.style.width = `${(scrollTop / height) * 100}%`;
+    console.log(scroller.style.width);
+  });
+
+  //   update the year dynamically
+};
